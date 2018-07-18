153ac4c0080ae3b8fb5fd7865e44285ad47d5ef8
Sergio <sergio@lidergorila.com>
2018-07-17 19:14:31 -0500

    suma de 2 numeros

--- hola_clase2.rb
+++ hola_clase2.rb
@@ -1,4 +1,6 @@
-print "Ingresa tu nombre: "
-name = gets.chomp
-puts "Hola \"#{name}\""
+print "Ingresa tu numero #1: "
+n1 = gets.chomp.to_i
+print "Ingresa tu numero #2: "
+n2 = gets.chomp.to_i
+puts n1 + n2
 #"Hola #{name}"
